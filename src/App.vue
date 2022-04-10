be804a23ceb1557d87e8889950988a34dd1830a0
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-11 00:16:15 +0100

    todo app

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div class="container">
       <AddTodo/>
+      <FilterTodos/>
       <br>
       <Todos/>
     </div>
@@ -13,12 +14,14 @@
 <script>
 import Todos from './components/Todos.vue'
 import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue'
 
 export default {
   name: 'app',
   components: {
     Todos,
-    AddTodo
+    AddTodo,
+    FilterTodos
   }
 }
 </script>
