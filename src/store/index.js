7b8cfc31af83ac090dc5c6977002ddbbbf0951fe
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-10 21:36:32 +0100

    todo app

--- src/store/index.js
+++ src/store/index.js
@@ -15,10 +15,16 @@ const store = createStore({
             const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
 
             commit('setTodos', response.data)
+        },
+        async addTodo({ commit }, title) {
+            const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false });
+
+            commit('newTodo', response.data);
         }
     },
     mutations: {
-        setTodos: (state, todos) => (state.todos = todos)
+        setTodos: (state, todos) => (state.todos = todos),
+        newTodo: (state, todo) => state.todos.unshift(todo),
     },
 
 });
