8bc6fa7cb1a7cf5b3cd71f346f614c39a8d98265
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-10 17:17:15 +0100

    todo app

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h3 class="head">Add Todo</h3>
+    <div class="add">
+      <form action="">
+        <input type="text" v-model="title" placeholder="Add Todo...">
+        <input type="submit" value="submit">
+      </form>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+input[type="text"]  {
+  width: 75%;
+  padding: 12px 2rem;
+  border: 1px rgb(199, 199, 219) solid;
+  outline: 0;
+  border-radius: 5px;
+  margin-right: 5px;
+}
+
+input[type="submit"] {
+  width: 20%;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: 700;
+  background: rgb(106, 106, 234);
+  padding: 10px;
+  border: 1px rgb(199, 199, 219) solid;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
